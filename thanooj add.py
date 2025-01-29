4fae47d8c7451491bfbb6ab1a54ca996894ac151
Thanooj1106 <thanoojkumarreddy@gmail.com>
2025-01-29 09:17:31 +0530

    Add files via upload

---
+++ thanooj add.py
@@ -0,0 +1,12 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/kavya/AppData/Local/Programs/Python/Python38/thanooj add.py
+Addition: 35
+Subtraction: 5
+Mulitiplication: 300
+Division: 1.3333333333333333
+Flooor division: 1
+Modulus: 5
+Exponentation: 32768000000000000000
+>>> 
