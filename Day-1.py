f8c90ec689f94b1192c94060fbfe0445808277c0
Thanooj1106 <thanoojkumarreddy@gmail.com>
2025-12-29 19:17:45 +0530

    Add files via upload
    Type casting, Operators, Round() function, input() function

---
+++ Day-1.py
@@ -0,0 +1,134 @@
+length = len("ThanoojKumar")
+print("Your name has "+" " + str(length)+" characters")
+print(type(length))
+num1 = int(input("Enter 1st number:"))
+num2 = int(input("Enter 2nd number:"))
+num3 = num1 + num2
+print(num3)
+num = input ("Enter a three digit number:")
+first_digit = num[0]
+second_digit = num[1]
+third_digit = num[2]
+print(int(first_digit)+int(second_digit)+int(third_digit))
+weight = input("Enter the weight:")
+height = input("Enter the height:")
+BMI = int(weight)/(float(height)*float(height))
+print(BMI)
+a = 5
+a +=2
+print(a)
+a = a/2
+print(a)
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
+d = int(input("d:"))
+e = int(a + b + c + d)
+print(e)
+a = 200
+print(a)
+a,b,c = 1,2,3
+print(a,b,c)
+a+=2
+print(a)
+c+=a
+print(c)
+c//=a
+print(c)
+a = 5
+print(a ==5)
+print(a >=5)
+print(a <=5)
+print(a>6)
+print(a<6)
+print((a+2) != 7)
+a,b = 1,2
+c = a+b
+print(c)
+c +=a
+print(c)
+c//=b
+print(c)
+a,b = 3,5
+c = False
+print(a<5 and b>4)
+print(a>5 or b==5)
+print(b)
+print(a<=5 and c)
+print(a <5 and c)
+print(a<5 or c)
+print(not(c))
+a = 5
+b = 4
+print(a>>2)
+print(a<<2)
+a,b = 3,5
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(~b)
+print(a<<2)
+print(a>>2)
+a = 26
+b = 23
+print(a&b)
+a,b = 17,24
+print(a|b)
+print(a^b)
+a = 45
+print(~a)
+a = 68
+print(a<<2)
+a = 56
+print(a>>3)
+a = 5
+b = 5
+print(a is b)
+print(a == b)
+print(id(a))
+print(id(b))
+a = 35
+b = 25
+print(a is b)
+print(id(a))
+print(id(b))
+print(a is not b)
+a = 25
+print(id(a))
+a+=1
+print(id(a))
+a = 34
+b = .0
+print(id(a))
+print(id(b))
+print(a is b)
+print(a is not b)
+A = "thanooj"
+print("j"in A)
+print("A"in A)
+print("ooj"in A)
+print("T"not in A)
+print("o"not in A)
+a = (1,2,3,4,5,32)
+print("6"not in a)
+print("8"in a)
+print(2 in a)
+print(round(14.33333,2))
+print(round(674,2))
+print(round(674,0))
+print(round(674,-1))
+print(round(674,-2))
+print(round(674,-3))
+print(round(674,-4))
+print(round(7.666666))
+print(type(round(7.6666)))
+print(type(round(7.6666,2)))
+print(round(7.666666,2))
+print(round(2.666557,0))
+print(round(7.5))
+print(round(6.75,1))
+print(round(6.85,1))
+print(round(674.1012,-1))
+print(round(1212,-2))
+print(round(467,-3))
