f15ebb673a48313e637a90b176f3cea215dd1d80
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-01-03 23:12:45 +0530

    Add files via upload

---
+++ Day-4 (Exercise practice).py
@@ -0,0 +1,44 @@
+#string concatenation and manipulation
+print("Hello"+"World")
+print("Hello"+" "+"World")
+print("Hello world\nHelloworld\nHelloworld\n")
+#input functions
+name = input("Enter your name:")
+print(f"Hello {name}")
+number = int(input("Enter a number:"))
+print(number)
+print("Hello" +" "+input("What is your name:"))
+print(input("Where are u from:"))
+print(input("What is your name:"))
+print("Hey"+" "+input("Your name:")+" "+"How are you")
+#variables
+a =input("Enter value of a:")
+print(a)
+age = int(input("Enter your age:"))
+print(age)
+height = int(input("Enter your height:"))
+print(height)
+#Datatypes
+a = 3
+print(type(a))
+a = 5.344
+print(type(a))
+a = 5j
+print(type(a))
+#type conversion
+a = 10
+b = 5.0
+c = 9j
+x =int(b)
+print(x)
+y = float(a)
+print(y)
+z =complex(a)
+print(z)
+name ="Thanooj"
+print(name[5])
+a = 1
+b = 2
+addn = a + b
+print(addn)
+
