7447bd724e85aed617e7fbe85b1dc9f863154e90
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-03-09 22:13:53 +0530

    Add files via upload

---
+++ Grading system using input.py
@@ -0,0 +1,16 @@
+marks=int(input("Enter the marks:"))
+if(marks>=91):
+    print("S grade")
+elif(marks>=81):
+    print("A grade")
+elif(marks>=71):
+    print("B grade")
+elif(marks>=61):
+    print("C grade")
+elif(marks>=55):
+    print("D grade")
+elif(marks>=50):
+    print("E grade")
+else:
+    print("Fail")
+           
