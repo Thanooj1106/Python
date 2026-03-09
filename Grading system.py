db03ee2abe77dd1e1035d0fee68aaff22a2b1d71
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-03-09 22:06:40 +0530

    Add files via upload

---
+++ Grading system.py
@@ -0,0 +1,15 @@
+marks=95
+if(marks>=91):
+    print("S grade")
+elif(marks>=81):
+    print("A grade")
+elif(marks>=71):
+    print("B grade")
+elif(marks>=61):
+    print("C grade")
+elif(marks>=55):
+    print("D grade")
+elif(marks>=50):
+    print("E grade")
+else:
+    print("Fail")
