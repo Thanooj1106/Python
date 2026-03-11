3b574299b119e10a9cb7c4613846551e380a301c
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-03-11 23:57:30 +0530

    Add files via upload

---
+++ Greatest of three numbers.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+c=int(input("Enter the number:"))
+if(a>b and a>c):
+    print("Greatest number is:",a)
+elif(b>a and b>c):
+    print("Greatest number is:",b)
+else:
+    print("Greatest number is:",c)
