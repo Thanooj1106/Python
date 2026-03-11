a0405d534abec10f20c44ef92ecc28bf4eeda45c
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-03-11 23:58:02 +0530

    Add files via upload

---
+++ Greatest of two numbers.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+if(a>b):
+    print("Greatest number is:",a)
+elif(b>a):
+    print("Greatest number is:",b)
+else:
+    print("Both are equal")
