661ee43303d292914d34ffff3054281339a19ff0
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-03-25 22:06:01 +0530

    Add files via upload

---
+++ comparison of two numbers.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+if a!=b:
+    if a>b:
+        print("a is greater")
+    else:
+        print("b is greater")
+else:
+    print("a and b are equal")
